b5ac136b1d0aa7f8dafb2b58dce1737a28f71399
Andrés Molinari <amolinarisalvo@gmail.com>
2023-09-27 17:41:09 -0300

    agregando funciones javascript v2

--- js/miarchivo_v2.js
+++ js/miarchivo_v2.js
@@ -30,6 +30,7 @@ const prote3 = new Producto('../img/Suplementos/WheyProtein/WheyProtein_3-remove
                             29.99);
 
 //VERIFICACION DE EXISTENCIA DEL CATALOGO DE PROTEINAS EN EL LOCALSTORAGE
+let cart = [];
 let catalogoDeProductos = []
 if(localStorage.getItem("catalogoDeProductos")){
     //hacer for of de catalogoDeProductos y pasarle new Producto
@@ -49,7 +50,7 @@ if(localStorage.getItem("catalogoDeProductos")){
 let productGrid = document.getElementById('productGrid');
 let selectOrden = document.getElementById("selectOrden");
 let buscador = document.getElementById("buscador");
-let coincidenciasDiv = document.getElementById("coincidencias");
+let coincidenciasDiv = document.getElementById("coincidenciasDiv");
 
 //FUNCIONES
 const maxDescriptionLength = 100; // Número máximo de caracteres para la descripción
@@ -58,42 +59,44 @@ function mostrarCatalogoDOM(array){
     //resetear el container
     productGrid.innerHTML = "";
 
-    array.forEach((product, index) => {
-        const productCard = document.createElement('div');
-        productCard.className = 'col-md-4 mb-4';
-    
-        // Limitar la descripción a un número máximo de caracteres
-        const truncatedDescription = product.description.length > maxDescriptionLength
-            ? product.description.slice(0, maxDescriptionLength) + '...'
-            : product.description;
-    
-        productCard.innerHTML = `
-        <div class="card">
-            <img src="${product.image}" class="card-img-top product-image mx-auto" alt="${product.title}">
-            <div class="card-body text-center">
-                <h5 class="card-title">${product.title}</h5>
-                <p class="card-text">${truncatedDescription}</p>
-                <p class="card-text">$${product.price.toFixed(2)}</p>
-                <div class="input-group mb-3">
-                    <span class="input-group-text">
-                        <button type="button" class="btn btn-sm" onclick="decreaseQuantity(${index})">-</button>
-                    </span>
-                    <input type="number" class="form-control" value="1" min="1" id="quantity${index}">
-                    <span class="input-group-text">
-                        <button type="button" class="btn btn-sm" onclick="increaseQuantity(${index})">+</button>
-                    </span>
-                    <button class="btn btn-warning" onclick="addToCart(${index})">Agregar</button>
+    if(array.length > 0){
+        array.forEach((product, index) => {
+            const productCard = document.createElement('div');
+            productCard.className = 'col-md-4 mb-4';
+        
+            // Limitar la descripción a un número máximo de caracteres
+            const truncatedDescription = product.description.length > maxDescriptionLength
+                ? product.description.slice(0, maxDescriptionLength) + '...'
+                : product.description;
+        
+            productCard.innerHTML = `
+            <div class="card">
+                <img src="${product.image}" class="card-img-top product-image mx-auto" alt="${product.title}">
+                <div class="card-body text-center">
+                    <h5 class="card-title">${product.title}</h5>
+                    <p class="card-text">${truncatedDescription}</p>
+                    <p class="card-text">$${product.price.toFixed(2)}</p>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">
+                            <button type="button" class="btn btn-sm" onclick="decreaseQuantity(${index})">-</button>
+                        </span>
+                        <input type="number" class="form-control" value="1" min="1" id="quantity${index}">
+                        <span class="input-group-text">
+                            <button type="button" class="btn btn-sm" onclick="increaseQuantity(${index})">+</button>
+                        </span>
+                        <button class="btn btn-warning" onclick="addToCart(${index})">Agregar</button>
+                    </div>
                 </div>
             </div>
-        </div>
-        `;
-    
-        // Establecer atributos de alto y ancho máximo a la imagen
-        const imageElement = productCard.querySelector('.product-image');
-        imageElement.setAttribute('style', 'max-width: 300px; max-height: 350px;');
-    
-        productGrid.appendChild(productCard);
-    });
+            `;
+        
+            // Establecer atributos de alto y ancho máximo a la imagen
+            const imageElement = productCard.querySelector('.product-image');
+            imageElement.setAttribute('style', 'max-width: 300px; max-height: 350px;');
+        
+            productGrid.appendChild(productCard);
+        });
+    }    
 }
 
 function listarProductosMayorMenorPrecio(array){
@@ -133,12 +136,26 @@ function buscarInfo(buscado,array){
             //includes cualquier coincidencia parcial en el string con includes
             return producto.title.toLowerCase().includes(buscado.toLowerCase()) || producto.description.toLowerCase().includes(buscado.toLowerCase())
         }
-    )
-    console.log(coincidencias.length);
+    )    
     //ternario para evaluar si coincidencias está vacio
     //ternario, tenemos varias instrucciones encerrar entre parentesis y separar por coma ,
-    coincidencias.length > 0 ? (mostrarCatalogoDOM(coincidencias), coincidenciasDiv.innerHTML ="") : (mostrarCatalogoDOM(array), coincidenciasDiv.innerHTML = `<h3>No hay coincidencias con su búsqueda, este es nuestro catálogo completo</h3>`) 
+    coincidencias.length > 0 ? (mostrarCatalogoDOM(coincidencias), coincidenciasDiv.innerHTML ="") : (mostrarCatalogoDOM(array), coincidenciasDiv.innerHTML = `<h3>No hay coincidencias con su búsqueda, este es nuestro catálogo completo</h3>`);
 }
+
+function addToCart(index) {
+    const quantity = parseInt(document.getElementById(`quantity${index}`).value);
+    if (quantity > 0) {
+        cart.push({
+            product: catalogoDeProductos[index],
+            quantity
+        });
+        //alert(`Se agregó ${quantity} ${products[index].title}(s) al carrito`);
+        document.getElementById(`quantity${index}`).value = 1;
+  
+        // Llamar a la función para actualizar el botón de carrito
+        actualizarBotonCarrito();
+    }
+  }
 
 //FUNCIONES AUXILIARES
 //Funcion para decrementar las cantidades de unidades a agregar al carrito
@@ -171,6 +188,32 @@ function iniciarSesion() {
 
     document.getElementById("usuario").value = "";
     document.getElementById("contrasena").value = "";
+}
+
+function actualizarBotonCarrito() {
+    const botonCarrito = document.getElementById('botonCarrito');
+    const carritoCantidad = document.getElementById('carritoCantidad');
+
+     // Calcular la suma de las cantidades de los productos en el carrito
+     let totalCantidad = 0;
+     for (const item of cart) {
+         totalCantidad += item.quantity;
+     }
+
+    // Verificar si hay productos en el carrito
+    if (totalCantidad > 0) {
+        // Si hay productos en el carrito, establecer la clase a "btn-danger"
+        botonCarrito.classList.remove('btn-secondary');
+        botonCarrito.classList.add('btn-danger');
+        carritoCantidad.textContent = ' ' + totalCantidad.toString();;
+    } else {
+        // Si no hay productos en el carrito, establecer la clase a "btn-secondary"
+        botonCarrito.classList.remove('btn-danger');
+        botonCarrito.classList.add('btn-secondary');
+    }
+
+    // Actualizar la cantidad de productos en el carrito    
+    //carritoCantidad.textContent = ' ' + cart.length.toString();
 }
 
 //EVENTOS DEL PROYECTO
